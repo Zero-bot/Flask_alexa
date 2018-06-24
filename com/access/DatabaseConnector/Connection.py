7f4a46fbd592dcfd95d26d3fb86497e75ebe4735
Marimuthu Mahalingam <marimuthu125@gmail.com>
2018-06-24 20:08:21 +0530

    removed comments

--- com/access/DatabaseConnector/Connection.py
+++ com/access/DatabaseConnector/Connection.py
@@ -24,27 +24,3 @@ class Connection:
             else:
                 print(err)
 
-# def get_config_data(value):
-#     return read_config('AWS', value)
-#
-#
-# def read_database_config():
-#     config_obj = {'user': get_config_data('user'),
-#                   'host': get_config_data('host'),
-#                   'database': get_config_data('database'),
-#                   'password': get_config_data('password')
-#                   }
-#     return config_obj
-#
-#
-# def connect_to_database():
-#     database_config = read_database_config()
-#     try:
-#         return connection.MySQLConnection(**database_config)
-#     except Error as err:
-#         if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-#             print("Something is wrong with your user name or password")
-#         elif err.errno == errorcode.ER_BAD_DB_ERROR:
-#             print("Database does not exist")
-#         else:
-#             print(err)
